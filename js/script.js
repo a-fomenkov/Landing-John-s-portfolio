be08cf9909a7f8230b146b8fd8e787ea6e203a72
A_Fomenkov <fomenkov235@gmail.com>
2021-05-04 19:55:39 +0300

    Completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu__body');
+const body = document.querySelector('body');
 
 iconMenu.addEventListener('click', function () {
 	iconMenu.classList.toggle('_active');
 	menuBody.classList.toggle('_active');
+	body.classList.toggle('lock');
 })

a2f50866b0e453fd7733c9ec041429794e8c8ff7
A_Fomenkov <fomenkov235@gmail.com>
2021-05-12 15:27:26 +0300

    create branch 'review'

---
+++ docs/js/script.js
@@ -0,0 +1,11 @@
+
+//BURGER
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body');
+const body = document.querySelector('body');
+
+iconMenu.addEventListener('click', function () {
+	iconMenu.classList.toggle('_active');
+	menuBody.classList.toggle('_active');
+	body.classList.toggle('_lock');
+})
